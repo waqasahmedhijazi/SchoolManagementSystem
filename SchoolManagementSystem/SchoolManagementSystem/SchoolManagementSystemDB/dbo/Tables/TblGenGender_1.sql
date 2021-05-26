17301bc5184d860eb65f47439f8138e5e1ac2b8f
Waqas Ahmed Hijazi <waqasahmedhijazi@gmail.com>
2021-05-27 03:37:31 +0500

    Submitting deleted files for tables views and storeprocedures.
    Submitting deleted files for tables views and storeprocedures.

--- SchoolManagementSystem/SchoolManagementSystem/SchoolManagementSystemDB/dbo/Tables/TblGenGender_1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[TblGenGender] (
-    [GenderID]  INT          IDENTITY (1, 1) NOT NULL,
-    [Gender]    VARCHAR (20) NULL,
-    [IsActive]  BIT          CONSTRAINT [DF_TblGenGender_IsActive] DEFAULT ((1)) NULL,
-    [IsDeleted] BIT          CONSTRAINT [DF_TblGenGender_IsDeleted] DEFAULT ((0)) NULL,
-    CONSTRAINT [PK_TblGenGender] PRIMARY KEY CLUSTERED ([GenderID] ASC)
-);
-
--- SchoolManagementSystem/SchoolManagementSystem/SchoolManagementSystemDB/dbo/Tables/TblGenMaritalStauts_1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[TblGenMaritalStauts] (
-    [MaritalStautsID] INT          IDENTITY (1, 1) NOT NULL,
-    [MaritalStauts]   VARCHAR (50) NULL,
-    [IsActive]        BIT          CONSTRAINT [DF_TblGenMaritalStauts_IsActive] DEFAULT ((1)) NULL,
-    [IsDeleted]       BIT          CONSTRAINT [DF_TblGenMaritalStauts_IsDeleted] DEFAULT ((0)) NULL,
-    CONSTRAINT [PK_TblGenMaritalStauts] PRIMARY KEY CLUSTERED ([MaritalStautsID] ASC)
-);
-
